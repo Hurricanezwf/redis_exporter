98dca88c0e96c3c58e694ef8b15fe4bedfa8936d
zhouwenfeng <zwf1094646850@gmail.com>
2025-03-21 17:09:45 +0800

    chore: fix build.sh

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -e
 
 rm -rf ./bin/
 mkdir -p ./bin/
-GOOS=linux  GOARCH=amd64 go build -ldflags "-s -w" -o ./bin/redis-exporter-v1.68-0.0.1-linux-amd64
-GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o ./bin/redis-exporter-v1.68-0.0.1-darwin-arm64
+GOOS=linux  GOARCH=amd64 go build -ldflags "-s -w" -o ./bin/redis-exporter-v1.68-0.0.2-linux-amd64
+GOOS=darwin GOARCH=arm64 go build -ldflags "-s -w" -o ./bin/redis-exporter-v1.68-0.0.2-darwin-arm64
